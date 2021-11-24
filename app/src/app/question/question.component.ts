f61ed972d2f2464f01e73733ad67763505659a76
箭内奨平 <jiangpingjiannei@gmail.com>
2021-11-24 18:14:11 +0900

    テスト用問題の作成

--- app/src/app/question/question.component.ts
+++ app/src/app/question/question.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UrlHandlingStrategy } from '@angular/router';
+import { Quiz, Choice, QUIZ_DATA} from 'src/app/const/quiz';
+import { QuizService } from '../services/quiz.service';
+
 
 @Component({
   selector: 'app-question',
@@ -8,14 +11,20 @@ import { UrlHandlingStrategy } from '@angular/router';
 })
 export class QuestionComponent implements OnInit {
 
+  // ヒントを表示するかどうか
   isDisplayHint: boolean = false;
-  constructor() { }
 
-  ngOnInit(): void { 
+  quiz?: Quiz;
+  quizList?: any;
+  quizCount?: number;
 
-  }
+//  サービスを使えるようにする
+  constructor(private quizservice:QuizService) {}
+
+  ngOnInit(): void {}
+
+  // 問題のヒントが表示する。
   clickHint():void{
     this.isDisplayHint = !this.isDisplayHint;
   }
-
 }
