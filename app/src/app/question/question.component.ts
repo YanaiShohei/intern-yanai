e88ab04cc24534368f4cbc2ee5569bc8e30c51fb
箭内奨平 <jiangpingjiannei@gmail.com>
2021-11-29 22:48:16 +0900

    問題画面にて選択肢を押すと次の問題が出てくる

--- app/src/app/question/question.component.ts
+++ app/src/app/question/question.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UrlHandlingStrategy } from '@angular/router';
+import { Quiz, Choice, QUIZ_DATA} from 'src/app/const/quiz';
+import { QuizService } from '../services/quiz.service';
 
 @Component({
   selector: 'app-question',
@@ -8,14 +10,26 @@ import { UrlHandlingStrategy } from '@angular/router';
 })
 export class QuestionComponent implements OnInit {
 
+  quiz?: Quiz;
+  quizList?: any;
+  quizCount?: number;
+
   isDisplayHint: boolean = false;
-  constructor() { }
 
-  ngOnInit(): void { 
+  //  サービスを使えるようにする
+  constructor(private quizservice:QuizService) {}
 
+  ngOnInit(): void {
+    this.quizCount = this.quizservice.quizCount;
+    this.quiz = this.quizservice.getQuiz();
   }
+    // 問題のヒントが表示する。
   clickHint():void{
     this.isDisplayHint = !this.isDisplayHint;
+  }
+  //
+  choiceAnswer(choice: Choice){
+    this.quiz = this.quizservice.nextQuiz();
   }
 
 }
